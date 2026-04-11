2b10e9629e8b4c7b79944ffd6376e82aee1a524a
Siddharth <siddharthkhathuria@gmail.com>
2026-04-12 02:58:43 +0530

    connect frontend to backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000',
+ baseURL: 'https://cogniveil-backend.onrender.com',
 });
 
 API.interceptors.request.use((config) => {
